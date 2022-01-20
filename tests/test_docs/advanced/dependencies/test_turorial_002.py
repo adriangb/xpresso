f38dfde058dca4dcbead8a6ffa0f1c4ab87e68cf
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2022-01-20 00:12:35 -0800

    test: mitigate flakey timing test by making sleeps longer
    We should skip this in all but the fastest CI in the future

--- tests/test_docs/advanced/dependencies/test_turorial_002.py
+++ tests/test_docs/advanced/dependencies/test_turorial_002.py
@@ -7,13 +7,14 @@ from docs_src.advanced.dependencies.tutorial_002 import app
 
 
 @pytest.mark.anyio
+@pytest.mark.slow
 async def test_get_slow_endpoint() -> None:
-    # this test may be flakey because it is susceptible to ovehread
-    # if it is, we should just ditch the timing part of it and only verify behavior
+    # unforuntely it is pretty hard to actually time this in tests without making
+    # really slow tests just for the sake of running timing on them
     async with httpx.AsyncClient(app=app, base_url="http://example.com") as client:
         start = time.time()
         resp = await client.get("/slow")
         stop = time.time()
         assert resp.status_code == 200, resp.content
         elapsed = stop - start
-        assert elapsed < 0.2
+        assert elapsed < 2
