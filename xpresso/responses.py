adc33a517718646925ab72cd6ffaf8e484a00196
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2022-01-29 17:25:25 -0600

    feat: re-export responses

--- xpresso/responses.py
+++ xpresso/responses.py
@@ -1,12 +1,33 @@
 from typing import Any, Iterable, Mapping, Optional, Union
 
 from pydantic import BaseModel, Field
-from starlette.responses import Response
+from starlette.responses import (
+    HTMLResponse,
+    JSONResponse,
+    PlainTextResponse,
+    Response,
+    StreamingResponse,
+)
 
 from xpresso._utils.asgi_scope_extension import XpressoASGIExtension
 from xpresso.exceptions import XpressoError
 from xpresso.openapi.models import Example, ResponseHeader
 from xpresso.requests import Request
+
+__all__ = (
+    "get_response",
+    "set_response",
+    "ResponseSpec",
+    "JsonResponseSpec",
+    "FileResponseSpec",
+    "HTMLResponseSpec",
+    "PlainTextResponseSpec",
+    "Response",
+    "JSONResponse",
+    "StreamingResponse",
+    "HTMLResponse",
+    "PlainTextResponse",
+)
 
 
 def get_response(request: Request) -> Response:
@@ -49,6 +70,11 @@ class FileResponseSpec(ResponseSpec):
 class HTMLResponseSpec(ResponseSpec):
     model: Any = Field(default=str)
     media_type: str = "text/html"
+
+
+class PlainTextResponseSpec(ResponseSpec):
+    model: str
+    media_type = "text/plain"
 
 
 Responses = Mapping[Union[int, str], Union[Iterable[ResponseSpec], ResponseSpec]]
