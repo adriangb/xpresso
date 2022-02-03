16d301103c7d38d510250b8fcba0c30984398d0c
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2022-02-03 00:49:26 -0600

    refactor: move openapi builder funcitons into modules

--- xpresso/openapi/_builder.py
+++ xpresso/openapi/_builder.py
@@ -1,45 +1,23 @@
-import inspect
-import sys
-from collections import ChainMap, Counter
-from typing import (
-    Any,
-    Callable,
-    Dict,
-    Iterable,
-    List,
-    Mapping,
-    Optional,
-    Set,
-    Tuple,
-    TypeVar,
-    Union,
-    cast,
-)
-
-if sys.version_info < (3, 9):
-    from typing_extensions import get_args, get_origin, get_type_hints
-else:
-    from typing import get_origin, get_args, get_type_hints
+from typing import Any, Dict, Iterable, List, Mapping, Optional, Set, Tuple, cast
 
 from di import BaseContainer
-from pydantic import BaseConfig
-from pydantic.fields import ModelField
-from pydantic.schema import field_schema, get_flat_models_from_field
-from pydantic.typing import NoneType
-from starlette.responses import Response
 from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 
 from xpresso._utils.routing import VisitedRoute
 from xpresso._utils.typing import get_model_name_map
 from xpresso.binders import dependants as binder_dependants
 from xpresso.openapi import models
+from xpresso.openapi._responses import (
+    get_response,
+    get_response_specs_from_return_type_hints,
+    merge_response_models,
+)
+from xpresso.openapi._security import get_security_models, get_security_scheme_name_map
 from xpresso.openapi.constants import REF_PREFIX
-from xpresso.responses import JsonResponseSpec, Responses, ResponseSpec
+from xpresso.responses import Responses, ResponseSpec
 from xpresso.routing.operation import Operation
 from xpresso.routing.pathitem import Path
 from xpresso.routing.router import Router
-
-T = TypeVar("T")
 
 ModelNameMap = Dict[type, str]
 
@@ -75,56 +53,6 @@ validation_error_response_definition = {
 }
 
 
-def get_response_models_from_return_hint(
-    return_hint: type,
-) -> List[ResponseSpec]:
-    res: Dict[str, ResponseSpec] = {}
-    description = ""
-    if get_origin(return_hint) is Union:
-        tps: List[Any] = []
-        for tp in get_args(return_hint):
-            if inspect.isclass(tp) and issubclass(tp, Response):
-                if tp.media_type is not None:
-                    res[tp.media_type] = ResponseSpec(
-                        description=description,
-                        media_type=tp.media_type,
-                    )
-            else:
-                if inspect.isclass(tp) and issubclass(tp, NoneType):
-                    return [ResponseSpec(description=description)]
-                tps.append(tp)
-        if tps:
-            res["application/json"] = JsonResponseSpec(
-                description=description,
-                model=Union[tuple(tps)],
-            )
-    else:
-        tp = return_hint
-        if inspect.isclass(tp) and issubclass(tp, Response):
-            if tp.media_type is not None:
-                res[tp.media_type] = ResponseSpec(
-                    description=description,
-                    media_type=tp.media_type,
-                )
-        else:
-            if inspect.isclass(tp) and issubclass(tp, NoneType):
-                return [ResponseSpec(description=description)]
-            res["application/json"] = JsonResponseSpec(
-                description=description,
-                model=tp,
-            )
-    return list(res.values()) or [ResponseSpec(description=description)]
-
-
-def get_response_specs_from_return_type_hints(
-    endpoint: Callable[..., Any]
-) -> List[ResponseSpec]:
-    hints = get_type_hints(endpoint, include_extras=True)
-    if "return" not in hints:
-        return []
-    return get_response_models_from_return_hint(hints["return"])
-
-
 def get_parameters(
     deps: List[binder_dependants.ParameterBinder],
     model_name_map: ModelNameMap,
@@ -153,83 +81,6 @@ def get_request_body(
             model_name_map=model_name_map, schemas=schemas
         )
     return models.RequestBody(content={})
-
-
-def get_response_schema(
-    type_: type, model_name_map: ModelNameMap, schemas: Dict[str, Any]
-) -> Dict[str, Any]:
-    field = ModelField.infer(
-        name="response",
-        value=...,
-        annotation=type_,
-        class_validators={},
-        config=BaseConfig,
-    )
-    model_name_map.update(
-        get_model_name_map(
-            get_flat_models_from_field(
-                field,
-                model_name_map.keys(),  # type: ignore[arg-type]
-            )
-        )
-    )
-    schema, refs, _ = field_schema(
-        field, by_alias=True, ref_prefix=REF_PREFIX, model_name_map=model_name_map
-    )
-    schemas.update(refs)
-    return schema
-
-
-def get_response(
-    spec: ResponseSpec,
-    model_name_map: ModelNameMap,
-    schemas: Dict[str, Any],
-) -> models.Response:
-    if spec.model is not None and spec.media_type is not None:
-        schema = get_response_schema(spec.model, model_name_map, schemas)
-        examples: Optional[Dict[str, models.Example]]
-        if spec.examples:
-            examples = {
-                n: ex if isinstance(ex, models.Example) else models.Example(value=ex)
-                for n, ex in spec.examples.items()
-            }
-        else:
-            examples = None
-        content = {spec.media_type: {"schema": schema, "examples": examples}}
-        model = models.Response(
-            description=spec.description, headers=spec.headers, content=content
-        )
-    else:
-        model = models.Response(
-            description=spec.description,
-            headers=spec.headers,
-            content=None,
-        )
-
-    return model
-
-
-def merge_response_models(
-    mods: Iterable[models.Response], default_description: Optional[str] = None
-) -> models.Response:
-    mods = list(mods)
-    if len(mods) == 1:
-        mod = next(iter(mods))
-        return models.Response(
-            description=mod.description or default_description or "",
-            headers=mod.headers,
-            content=mod.content,
-        )
-    descriptions: List[str] = []
-    for m in mods:
-        if m.content and m.description:
-            descriptions.append(f"- {next(iter(m.content.keys()))}: {m.description}")
-    description = "\n".join(descriptions)
-    if default_description:
-        description = description or default_description
-    headers = dict(ChainMap(*((m.headers or {}) for m in mods))) or None
-    content = dict(ChainMap(*((m.content or {}) for m in mods))) or None
-    return models.Response(description=description, headers=headers, content=content)
 
 
 def get_responses(
@@ -442,43 +293,6 @@ def get_flat_models(routes: Routes) -> Set[type]:
                     openapi = dep.openapi
                     res.update(openapi.get_models())
     return res
-
-
-def get_security_models(
-    routes: Routes, container: BaseContainer
-) -> Mapping[binder_dependants.SecurityBinder, models.SecurityScheme]:
-    res: Dict[binder_dependants.SecurityBinder, models.SecurityScheme] = {}
-    for _, operations in routes.values():
-        for operation in operations.values():
-            dependant = operation.dependant
-            flat_dependencies = dependant.get_flat_subdependants()
-            for dep in flat_dependencies:
-                if isinstance(
-                    dep,
-                    binder_dependants.SecurityBinder,
-                ):
-                    res[dep] = dep.construct_model(container)
-    return res
-
-
-def get_security_scheme_name_map(
-    models: Mapping[binder_dependants.SecurityBinder, models.SecurityScheme]
-) -> Mapping[models.SecurityScheme, str]:
-    scheme_names = {scheme: binder.scheme_name for binder, scheme in models.items()}
-    name_counter = Counter(scheme_names.values())
-    schemes_with_duplicate_names = [
-        scheme for scheme in scheme_names if name_counter[scheme_names[scheme]] > 1
-    ]
-    # sort in reverse order so that APIKey(name="key1") gets assigned the deduped name "APIKey_1"
-    for scheme in sorted(
-        schemes_with_duplicate_names,
-        key=lambda scheme: tuple(scheme.dict().items()),
-        reverse=True,
-    ):
-        name = scheme_names[scheme]
-        scheme_names[scheme] += f"_{name_counter[name]}"
-        name_counter[name] -= 1
-    return scheme_names
 
 
 def genrate_openapi(
