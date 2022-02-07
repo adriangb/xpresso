9c764703067c77cb409c3bdfebc7c18f5f9c6d35
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2022-02-07 01:19:17 -0600

    Add test for overriding marker callable

---
+++ tests/test_dependency_overrides.py
@@ -0,0 +1,21 @@
+from xpresso import App, Depends, Path
+from xpresso.testclient import TestClient
+from xpresso.typing import Annotated
+
+
+def test_override_with_marker() -> None:
+    def dep() -> int:
+        return 1
+
+    async def endpoint(v: Annotated[int, Depends(dep)]) -> int:
+        return v
+
+    app = App([Path("/", get=endpoint)])
+
+    app.dependency_overrides[dep] = lambda: 2
+
+    client = TestClient(app)
+
+    resp = client.get("/")
+    assert resp.status_code == 200, resp.content
+    assert resp.json() == 2
