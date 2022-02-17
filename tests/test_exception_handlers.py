675c222e1f77eec24c85d2ba2ae5173627d34991
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2022-02-17 17:08:42 -0600

    test: add exception handler tests

--- tests/test_exception_handlers.py
+++ tests/test_exception_handlers.py
@@ -1,4 +1,7 @@
-from xpresso import App, HTTPException, Path, Request
+import pytest
+
+from xpresso import App, FromPath, HTTPException, Path, Request
+from xpresso.exceptions import RequestValidationError
 from xpresso.responses import JSONResponse
 from xpresso.testclient import TestClient
 
@@ -40,3 +43,73 @@ def test_override_base_error_handler() -> None:
     resp = client.get("/raise-500")
     assert resp.status_code == 500, resp.content
     assert resp.json() == {"detail": "Custom Server Error from HTTPException(500)"}
+
+
+def http_exception_handler(request: Request, exception: Exception):
+    return JSONResponse({"exception": "http-exception"})
+
+
+def request_validation_exception_handler(request: Request, exception: Exception):
+    return JSONResponse({"exception": "request-validation"})
+
+
+def server_error_exception_handler(request: Request, exception: Exception):
+    raise exception
+
+
+def route_with_http_exception():
+    raise HTTPException(status_code=400)
+
+
+def route_with_request_validation_exception(param: FromPath[int]):
+    pass  # pragma: no cover
+
+
+class UnhanldedException(Exception):
+    pass
+
+
+def route_with_server_error():
+    raise UnhanldedException
+
+
+app = App(
+    routes=[
+        Path("/server-error", get=route_with_server_error),
+        Path(
+            "/request-validation/{param}/", get=route_with_request_validation_exception
+        ),
+        Path("/http-exception", get=route_with_http_exception),
+    ],
+    exception_handlers={
+        HTTPException: http_exception_handler,
+        RequestValidationError: request_validation_exception_handler,
+        Exception: server_error_exception_handler,
+    },
+)
+
+client = TestClient(app)
+
+
+def test_override_http_exception():
+    response = client.get("/http-exception")
+    assert response.status_code == 200
+    assert response.json() == {"exception": "http-exception"}
+
+
+def test_override_request_validation_exception():
+    response = client.get("/request-validation/invalid")
+    assert response.status_code == 200
+    assert response.json() == {"exception": "request-validation"}
+
+
+def test_override_server_error_exception_raises():
+    with pytest.raises(UnhanldedException):
+        client.get("/server-error")
+
+
+def test_override_server_error_exception_response():
+    client = TestClient(app, raise_server_exceptions=False)
+    response = client.get("/server-error")
+    assert response.status_code == 500
+    assert response.json() == {"exception": "server-error"}
