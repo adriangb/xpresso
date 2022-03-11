1108ed5bafd78a3294031213e3713fc38a89f34b
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2022-03-11 14:08:29 -0500

    fix: support Starlette versions down to 0.17.1

--- xpresso/exception_handlers.py
+++ xpresso/exception_handlers.py
@@ -22,7 +22,7 @@ class ExcHandler:
 
 async def http_exception_handler(request: Request, exc: HTTPException) -> JSONResponse:
     return JSONResponse(
-        {"detail": exc.detail}, status_code=exc.status_code, headers=exc.headers
+        {"detail": exc.detail}, status_code=exc.status_code, headers=getattr(exc, "headers", None) or {}
     )
 
 
